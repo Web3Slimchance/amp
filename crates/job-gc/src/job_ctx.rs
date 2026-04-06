e9131c2d3c2656cebdfbbc3919fa02cd468f297e
Lorenzo Delgado <lorenzo@edgeandnode.com>
2026-04-06 11:29:07 +0200

    chore(job-gc): derive Debug and Clone for job Context (#2085)
    Enable cloning of GC job context to support shared ownership across concurrent job execution.
    
    - Add `Debug` and `Clone` derives to `Context` struct
    
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- crates/job-gc/src/job_ctx.rs
+++ crates/job-gc/src/job_ctx.rs
@@ -4,6 +4,7 @@ use amp_data_store::DataStore;
 use monitoring::telemetry::metrics::Meter;
 
 /// Dependencies required to execute a GC job.
+#[derive(Debug, Clone)]
 pub struct Context {
     /// Connection to object storage (S3/GCS/local FS).
     pub data_store: DataStore,
